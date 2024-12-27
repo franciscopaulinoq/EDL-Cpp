367d191c024fca250fc3c3186c378dc43757a28d
Francisco Paulino <franciscopaulino.q@gmail.com>
2024-12-27 17:52:58 -0300

    refactor(TrappedMouse): criando migrando a lógica do labirinto para a classe Maze

---
+++ TrappedMouse/include/Maze.h
@@ -0,0 +1,185 @@
+#ifndef MAZE_H
+#define MAZE_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <windows.h>
+#include "Cell.h"
+#include "Stack.h"
+
+class Maze
+{
+private:
+    Cell currentCell, exitCell, entryCell;
+    static constexpr char exitMarker = 'e', entryMarker = 'm', visited = '.', passage = '0', wall = '1';
+    Stack<Cell> mazeStack;
+    char **maze;
+    int rows, cols;
+
+    void printMaze() const;
+    bool isValidMove(int line, int col) const;
+    void clearScreen() const;
+
+public:
+    Maze();
+    ~Maze();
+
+    bool loadFromFile(const std::string &filename);
+
+    friend std::ostream &operator<<(std::ostream &os, const Maze &maze);
+
+    void exitMaze();
+};
+
+Maze::Maze() : maze(nullptr), rows(0), cols(0) {}
+
+Maze::~Maze()
+{
+    if (maze)
+    {
+        for (int i = 0; i < rows; i++)
+        {
+            delete[] maze[i];
+        }
+        delete[] maze;
+    }
+}
+
+bool Maze::loadFromFile(const std::string &filename)
+{
+    std::ifstream arquivo(filename);
+    if (!arquivo.is_open())
+    {
+        std::cerr << "Erro ao abrir o arquivo de entrada.\n";
+        return false;
+    }
+
+    std::string linha;
+    std::getline(arquivo, linha);
+    std::stringstream ss(linha);
+    ss >> rows >> cols;
+
+    maze = new char *[rows];
+    for (int i = 0; i < rows; i++)
+    {
+        maze[i] = new char[cols];
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        std::getline(arquivo, linha);
+        std::stringstream ss(linha);
+        for (int j = 0; j < cols; j++)
+        {
+            char valor;
+            ss >> valor;
+
+            if (valor != passage && valor != wall && valor != entryMarker && valor != exitMarker)
+            {
+                std::cerr << "Invalid value found: " << valor << " at position (" << i << ", " << j << ")\n";
+                return false;
+            }
+
+            maze[i][j] = valor;
+
+            if (valor == entryMarker)
+            {
+                entryCell = Cell(i, j);
+            }
+            else if (valor == exitMarker)
+            {
+                exitCell = Cell(i, j);
+            }
+        }
+    }
+
+    currentCell = entryCell;
+    return true;
+}
+
+bool Maze::isValidMove(int line, int col) const
+{
+    return line >= 0 && line < rows && col >= 0 && col < cols &&
+           (maze[line][col] == passage || maze[line][col] == exitMarker);
+}
+
+void Maze::clearScreen() const
+{
+    COORD cursorPosition = {0, 0};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
+}
+
+void Maze::printMaze() const
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            char c = maze[i][j];
+            switch (c)
+            {
+            case wall:
+                std::cout << "# ";
+                break;
+            case entryMarker:
+                std::cout << "\033[1;32mM\033[0m ";
+                break;
+            case exitMarker:
+                std::cout << "\033[1;33mE\033[0m ";
+                break;
+            case visited:
+                std::cout << "  ";
+                break;
+            default:
+                std::cout << "  ";
+                break;
+            }
+        }
+        std::cout << std::endl;
+    }
+}
+void Maze::exitMaze()
+{
+    mazeStack.push(currentCell);
+
+    while (!(currentCell == exitCell) && !mazeStack.empty())
+    {
+        const int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (auto &dir : directions)
+        {
+            int newLine = currentCell.getX() + dir[0];
+            int newCol = currentCell.getY() + dir[1];
+            if (isValidMove(newLine, newCol))
+            {
+                mazeStack.push(Cell(newLine, newCol));
+            }
+        }
+
+        maze[currentCell.getX()][currentCell.getY()] = visited;
+
+        if (!mazeStack.empty())
+        {
+            currentCell = mazeStack.top();
+            mazeStack.pop();
+            maze[currentCell.getX()][currentCell.getY()] = entryMarker;
+        }
+
+        clearScreen();
+        printMaze();
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    }
+
+    if (currentCell == exitCell)
+    {
+        std::cout << "\033[1;32m\nSAIDA ENCONTRADA!\033[0m" << std::endl;
+    }
+    else
+    {
+        std::cout << "\033[1;31m\nNAO HA SAIDA POSSIVEL!\033[0m" << std::endl;
+    }
+}
+
+#endif
--- TrappedMouse/source/main.cpp
+++ TrappedMouse/source/main.cpp
@@ -1,50 +1,6 @@
-#include <fstream>
 #include <iostream>
-#include <sstream>
-#include <string>
-#include <thread>
 #include <windows.h>
-#include "Cell.h"
-#include "Stack.h"
-
-using namespace std;
-
-bool isValidMove(int line, int col, char **matrix)
-{
-    return matrix[line][col] == '0' || matrix[line][col] == 'e';
-}
-
-void printMaze(char **matrix, int line, int col)
-{
-    for (int i = 0; i < line; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            switch (matrix[i][j])
-            {
-            case '1':
-                cout << "# ";
-                break;
-            case 'm':
-                cout << "\033[1;32mM\033[0m ";
-                break;
-            case 'e':
-                cout << "\033[1;33mE\033[0m ";
-                break;
-            default:
-                cout << "  ";
-                break;
-            }
-        }
-        cout << "\n";
-    }
-}
-
-void clearScreen()
-{
-    COORD cursorPosition = {0, 0};
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursorPosition);
-}
+#include "Maze.h"
 
 void ShowConsoleCursor(bool showFlag)
 {
@@ -55,116 +11,19 @@ void ShowConsoleCursor(bool showFlag)
     SetConsoleCursorInfo(out, &cursorInfo);
 }
 
-void movePlayer(char **matrix, Cell &currentCell, Stack<Cell> &cellStack)
-{
-    const int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-    for (auto &dir : directions)
-    {
-        int newLine = currentCell.getX() + dir[0];
-        int newCol = currentCell.getY() + dir[1];
-        if (isValidMove(newLine, newCol, matrix))
-        {
-            cellStack.emplace(newLine, newCol);
-        }
-    }
-}
-
-bool loadMaze(const string &filename, char **&matrix, int &line, int &col, Cell &start, Cell &exit)
-{
-    ifstream arquivo(filename);
-    if (!arquivo.is_open())
-    {
-        cerr << "Erro ao abrir o arquivo de entrada.\n";
-        return false;
-    }
-
-    string linha;
-    getline(arquivo, linha);
-    stringstream ss(linha);
-    ss >> line >> col;
-
-    matrix = new char *[line];
-    for (int i = 0; i < line; i++)
-    {
-        matrix[i] = new char[col];
-    }
-
-    for (int i = 0; i < line; i++)
-    {
-        getline(arquivo, linha);
-        stringstream ss(linha);
-        for (int j = 0; j < col; j++)
-        {
-            char valor;
-            ss >> valor;
-
-            if (valor != '0' && valor != '1' && valor != 'm' && valor != 'e')
-            {
-                cerr << "Invalid value found: " << valor << " at position (" << i << ", " << j << ")\n";
-                return false;
-            }
-
-            if (valor == 'm')
-            {
-                start = Cell(i, j);
-            }
-            else if (valor == 'e')
-            {
-                exit = Cell(i, j);
-            }
-            matrix[i][j] = valor;
-        }
-    }
-    return true;
-}
-
 int main()
 {
-    char **matrix;
-    int line, col;
-    Cell currentCell, exitCell;
+    Maze maze;
 
-    if (!loadMaze("resources/input.txt", matrix, line, col, currentCell, exitCell))
+    if (!maze.loadFromFile("resources/input.txt"))
     {
         return 1;
     }
 
-    Stack<Cell> cellStack;
-    cellStack.push(currentCell);
-
-    system("cls");
     ShowConsoleCursor(false);
-    printMaze(matrix, line, col);
+    system("cls");
 
-    while (!(currentCell == exitCell) && !cellStack.empty())
-    {
-        movePlayer(matrix, currentCell, cellStack);
-
-        matrix[currentCell.getX()][currentCell.getY()] = '.';
-        currentCell = cellStack.top();
-        cellStack.pop();
-        matrix[currentCell.getX()][currentCell.getY()] = 'm';
-
-        clearScreen();
-        printMaze(matrix, line, col);
-        this_thread::sleep_for(chrono::milliseconds(500));
-    }
-
-    if (currentCell == exitCell)
-    {
-        cout << "\033[1;32m\nSAIDA ENCONTRADA!\033[0m" << endl;
-    }
-    else
-    {
-        cout << "\033[1;31m\nNAO HA SAIDA POSSIVEL!\033[0m" << endl;
-    }
-
-    for (int i = 0; i < line; i++)
-    {
-        delete[] matrix[i];
-    }
-    delete[] matrix;
+    maze.exitMaze();
 
     return 0;
 }
